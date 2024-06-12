833715f774a8955236b033b513edae06b175aa13
Behruz <bexruzbxdrv@gmail.com>
2024-06-12 16:42:58 +0500

    first commit

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from app.views import index
+from app.views import index, product_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', index, name='index')
+    path('index/', index, name='index'),
+    path('product-detail/<int:product_id>', product_detail, name='product_detail')
 ]
